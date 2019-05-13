eed518aebbd3c778ecf7e1c2f243d7b857b0c151
lackmorrison <mr.deadman.li@gmail.com>
2019-05-13 17:13:46 +0300

    log message

--- src/main/java/org/coindirect/centrifuge/java/Centrifugo.java
+++ src/main/java/org/coindirect/centrifuge/java/Centrifugo.java
@@ -588,7 +588,7 @@ public class Centrifugo {
          */
         @Override
         public void onMessage(final String message) {
-            System.out.println("centrifugo onMessage " + message);
+            Log.info("centrifugo onMessage " + message);
             try {
                 Object object = new JSONTokener(message).nextValue();
                 if (object instanceof JSONObject) {
